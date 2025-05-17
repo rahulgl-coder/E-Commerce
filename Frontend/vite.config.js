b0cf51a043630d923ab25c9782ef1894d13bce73
rahulgl-coder <rahulnandhu0521@gmail>
2025-05-17 20:01:34 +0530

    after changing config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['e-commerce-5-kdu1.onrender.com'] // ✅ Add your Render domain here
   }
 })
