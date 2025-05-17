5a80d11186f62d359fd351eeea1feafbd0ace20b
rahulgl-coder <rahulnandhu0521@gmail>
2025-05-17 19:57:14 +0530

    AFTER editing config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173
+  }
 })
