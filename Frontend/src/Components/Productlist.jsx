af7d463f74ae46b5460b2ebe59c553c58478bfc0
rahulgl-coder <rahulnandhu0521@gmail>
2025-05-19 11:40:09 +0530

    AFTER changing

--- Frontend/src/Components/Productlist.jsx
+++ Frontend/src/Components/Productlist.jsx
@@ -23,7 +23,7 @@ export default function Productlist() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/auth/products`{
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/auth/products`,{
   withCredentials: true
 });
      console.log(res);
